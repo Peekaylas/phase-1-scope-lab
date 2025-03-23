fc607666c26f57e3fde23a151d690332e6a2413f
Philip Katiku <peekaylas@gmail.com>
2025-03-23 21:26:26 +0300

    problem

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+customerName = 'bob';
+const leastFavoriteCustomer = 'not bob';
+
+function upperCaseCustomerName() {
+    let customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    globalThis.bestCustomer = 'not bob';
+    return globalThis.bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    globalThis.bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob';
+}
+
+console.log(customerName);
+upperCaseCustomerName();
+console.log(customerName);
+setBestCustomer();
+console.log(globalThis.bestCustomer);
+overwriteBestCustomer();
+console.log(globalThis.bestCustomer);
+console.log(leastFavoriteCustomer);
+changeLeastFavoriteCustomer();
